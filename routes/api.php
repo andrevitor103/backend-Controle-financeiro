32e6cd9616a6d251a0e90519dad8849dc70f6a0e
André Vitor <andrevitor103@gmail.com>
2021-08-22 13:45:48 -0300

    Adicao de id na rota e na rotina de filter line

--- routes/api.php
+++ routes/api.php
@@ -55,7 +55,7 @@ Route::post('despesa/create-categoria', [CategoriaController::class, 'store']);
 Route::post('create-usuario', [UsuarioController::class, 'store']);
 Route::post('user/confirm-login', [UsuarioController::class, 'login']);
 Route::post('dashboard/filter/{id}', [DespesaController::class, 'filterDashboard']);
-Route::post('dashboard/filter-line', [DespesaController::class, 'filterDashboardLine']);
+Route::post('dashboard/filter-line/{id}', [DespesaController::class, 'filterDashboardLine']);
 Route::post('despesas-filter/{id}', [DespesaController::class, 'filterIndex']);
 
 Route::delete('despesas/delete/{id}', [DespesaController::class, 'destroy']);
